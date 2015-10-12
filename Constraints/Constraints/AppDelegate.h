f624e1e053d36cf56a6614962915fd71a2ebfe5f
Arthurisme <is@live.ca>
2015-10-12 08:51:37 -0400

    Constraints

---
+++ Constraints/Constraints/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Constraints
+//
+//  Created by Arthur on 2015-10-12.
+//  Copyright © 2015 Arthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Constraints/Constraints/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Constraints
+//
+//  Created by Arthur on 2015-10-12.
+//  Copyright © 2015 Arthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
