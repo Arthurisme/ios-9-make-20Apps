720da1ddbf864a168181e436d328d53571426cec
Arthurisme <is@live.ca>
2015-11-01 12:22:29 -0500

    animation

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    var timer = NSTimer()
+    var Counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+ 
+    @IBAction func PlayAnimation(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: Selector("PerformAnimatin"), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    func PerformAnimatin(){
+        
+        Counter++
+        
+        if Counter == 16{
+            Counter = 1
+        }
+        
+        ImageView.image = UIImage(named: "win_\(Counter).png")
+        
+    }
 }
 
