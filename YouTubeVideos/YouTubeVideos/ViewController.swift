172f9480825fb6e1a34d5ea09b4fb54730846438
Arthurisme <is@live.ca>
2015-11-03 16:42:55 -0500

    Audio

--- YouTubeVideos/YouTubeVideos/ViewController.swift
+++ YouTubeVideos/YouTubeVideos/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+    @IBOutlet weak var webView1: UIWebView!
+    
+    @IBOutlet weak var WebView2: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let EmbedCode = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/iwgfEQk4p4U\" frameborder=\"0\" allowfullscreen></iframe>"
+        
+        webView1.loadHTMLString(EmbedCode, baseURL: nil)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func searchBarSearchButtonClicked(searchbar: UISearchBar){
+        
+        searchbar.resignFirstResponder()
+        let EmbedCode2 = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/" + (searchbar.text!) + "\" frameborder=\"0\" allowfullscreen></iframe>"
+        
+        webView1.loadHTMLString(EmbedCode2, baseURL: nil)
+        
     }
 
 
