571d60151b5cab245f43d915c4b21bde1648248b
Arthurisme <is@live.ca>
2015-11-05 12:20:38 -0500

    tableview

---
+++ CarSpotter/CarSpotter/AddViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddViewController.swift
+//  CarSpotter
+//
+//  Created by Arthur on 2015-11-05.
+//  Copyright © 2015 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    @IBOutlet weak var CarMakeTextField: UITextField!
+    @IBOutlet weak var CarModelTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CarSpotter/CarSpotter/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  CarSpotter
+//
+//  Created by Arthur on 2015-11-05.
+//  Copyright © 2015 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var CarMakeLabel: UILabel!
+    @IBOutlet weak var CarModelLabel: UILabel!
+    @IBOutlet weak var CarYearLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
